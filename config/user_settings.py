719e4190dbbeb1241c9fc7bc1028876dc4ad9d6d
Andrii Pryz <well.i.beta@gmail.com>
2014-02-11 20:39:00 +0200

    change user_setting

--- config/user_settings.py
+++ config/user_settings.py
@@ -5,15 +5,14 @@ Empty '' string means setting "off"
 user_settings = {
     # blank line indicates a development server 127.0.0.1:8000
     'DOMAIN_NAME': '',#'http://mydomain.com/',#'http://demo.mysmile.com.ua/'
-    'PHONE': '+38 000 000 00 00',
-    'EMAIL': 'my-email@email.com',
+    'PHONE': '000 000 000 00 00', #  http://en.wikipedia.org/wiki/Telephone_numbering_plan 
+    'EMAIL': 'myemail@email.com',
     'SKYPE': 'myskype',
 
     # go to the http://www.google.com/analytics/ to get the GOOGLE_ANALITYCS_CODE
     # it's unique number like 'UA-1111111-1'
-    # empty '' string if off code
     'GOOGLE_ANALITYCS_CODE': '',
-    'MAX_INNERLINK_HISTORY': 4 # for inner_type page only <= 6
+    'MAX_INNERLINK_HISTORY': 4 # for inner_type page only <= MAX_INNERLINK_HISTORY
 }
 
 
