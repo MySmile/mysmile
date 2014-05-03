aa3806849df3c309362b82c298bb8b34e0b2ead4
Andrii Pryz <well.i.beta@gmail.com>
2014-05-04 00:44:49 +0300

    fix stupid KeyError in app.sitemap

--- apps/sitemap/views.py
+++ apps/sitemap/views.py
@@ -13,7 +13,7 @@ def SitemapXML(request):
     <urlset xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9">'
     langs_and_slugs = Page_translation.objects.filter(page__status=1).values('lang', 'page__slug',           'updated_at').order_by('lang')
     for item in langs_and_slugs:
-        url = app_settings['DOMAIN_NAME'] + item['lang'] + '/' + item['page__slug'] + '.html'
+        url = app_settings['DOMAIN'] + item['lang'] + '/' + item['page__slug'] + '.html'
         modified = item['updated_at'].strftime('%Y-%m-%d')
         xml += createNode(url, modified)
     xml += '</urlset>'
