96b53217c52f02cfd696a6694865d4c43fdf0603
Andrii Pryz <well.i.beta@gmail.com>
2014-05-17 05:39:24 +0300

    add adaptive protect

--- apps/pages/templatetags/email2img.py
+++ apps/pages/templatetags/email2img.py
@@ -4,16 +4,27 @@ from PIL import Image, ImageDraw, ImageFont
 from django import template
 register = template.Library()
 
-from mysmile.settings.main import STATIC_ROOT
+from mysmile.settings.main import STATIC_ROOT, BASE_DIR, DEBUG
  
 
 @register.filter(name='email2img')
 def email2img(email):
     """ protect email via img
     """
-    if os.path.isfile(STATIC_ROOT+'static/images/email2img.png'):
-        return """<img src="/static/images/email2img.png" alt="email">"""
-    else:
+    try:
+        os.path.isfile(STATIC_ROOT+'static/images/email2img.png')
+        if DEBUG:
+            email_mod_time = round(os.stat(BASE_DIR+'config/local.py').st_mtime)
+        else:
+            email_mod_time = round(os.stat(BASE_DIR+'config/production.py').st_mtime)
+            
+        email2img_mod_time = round(os.stat(STATIC_ROOT+'static/images/email2img.png').st_mtime)
+        if email_mod_time > email2img_mod_time:
+            raise IOError
+        else:
+            return """<img src="/static/images/email2img.png" alt="email">"""
+    
+    except (OSError, IOError):
         color_mode="RGBA"
         background_color=(0,0,0,0) # full transparent
         fontfile = STATIC_ROOT+'static/fonts/TimesNewRomanCE.ttf'
@@ -33,6 +44,5 @@ def email2img(email):
             return '<a href="mailto:'+email+'">'+email+'</a>'
         else:
             return """<img src="/static/images/email2img.png" alt="email" />"""
-
 
 
