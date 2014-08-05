09b44ac73cc7432e8bde90593ce760c034e5fb0d
Andrii Pryz <well.i.beta@gmail.com>
2014-08-05 18:53:25 +0300

    add exception

--- apps/pages/managers.py
+++ apps/pages/managers.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.cache import cache
 from django.core import signing
+from django.http import Http404
 
 from mysmile.settings.main import LANGUAGES
 from apps.pages.models import Page, Page_translation
@@ -35,8 +36,10 @@ class PagesManager(SettingsManager):
         return inner_nav
 
     def get_page(self, lang, slug):
-        page = Page_translation.objects.filter(lang=lang, page__ptype__in = [Page.PTYPE_INNER,Page.PTYPE_MENU,Page.PTYPE_MENU_API], page__status=Page.STATUS_PUBLISHED, page__slug=slug).values('page__color', 'page__photo', 'menu', 'name', 'col_central', 'col_right', 'youtube', 'col_bottom_1', 'col_bottom_2', 'col_bottom_3', 'photo_alt', 'photo_description', 'meta_title', 'meta_description', 'meta_keywords', 'page__ptype')[0]
-
+        try:
+            page = Page_translation.objects.filter(lang=lang, page__ptype__in = [Page.PTYPE_INNER,Page.PTYPE_MENU,Page.PTYPE_MENU_API], page__status=Page.STATUS_PUBLISHED, page__slug=slug).values('page__color', 'page__photo', 'menu', 'name', 'col_central', 'col_right', 'youtube', 'col_bottom_1', 'col_bottom_2', 'col_bottom_3', 'photo_alt', 'photo_description', 'meta_title', 'meta_description', 'meta_keywords', 'page__ptype')[0]
+        except IndexError:
+            raise Http404
         cols = ['col_bottom_1', 'col_bottom_2', 'col_bottom_3']  # some processing of the columns...
         page['bottom_cols'] = [page.pop(item) for item in cols if page[item]]
 
