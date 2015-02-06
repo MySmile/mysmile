d9f1da92a498364992eae7f795a8bf163674021b
Andrii Pryz <well.i.beta@gmail.com>
2015-02-06 14:05:52 +0200

    update

--- apps/pages/sql/page_translation.sqlite3.sql
+++ apps/pages/sql/page_translation.sqlite3.sql
@@ -1,4 +1,4 @@
-INSERT INTO page_translation (page_id, lang, menu, name, col_central, youtube, col_right, col_bottom_1, col_bottom_2, 
+INSERT INTO Page_translation (page_id, lang, menu, name, col_central, youtube, col_right, col_bottom_1, col_bottom_2, 
 col_bottom_3, meta_title, meta_description, meta_keywords, photo_alt, photo_description, created_at, updated_at) 
 VALUES (1, 'en', 'Home', 'Home page', 'Welcome to MySmile!', '', 'Lorem ipsum', 'Lorem ipsum', 'Lorem ipsum', 'Lorem ipsum', 'Home', 'description', 'keywords', 'alt photo', 'photo description', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP); 
 
