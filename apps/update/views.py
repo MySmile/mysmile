1831e97a518a89680458638bde42103016bf33d7
Andrii Pryz <well.i.beta@gmail.com>
2015-10-05 11:01:26 +0200

    remove requests package from requirements

--- apps/update/views.py
+++ apps/update/views.py
@@ -1,4 +1,5 @@
-import requests
+import json
+import urllib
 
 from django.contrib import messages
 from django.views.generic.base import RedirectView
@@ -13,7 +14,8 @@ class CheckUpdate(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         try:
             url = 'https://api.github.com/repos/MySmile/mysmile/releases/latest'
-            latest_release = requests.get(url).json()
+            r = urllib.request.urlopen(url)
+            latest_release = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
             version = latest_release.get('tag_name')[1::]
             if version > settings.MYSMILE_VERSION:
                 msg = 'New version ' + version + ' is avaliable! <a href="' + \
--- config/mysmile.py
+++ config/mysmile.py
@@ -2,7 +2,7 @@
 from django.conf import settings
 from apps.preferences.models import Preferences
 
-MYSMILE_VERSION = '0.7.0'
+MYSMILE_VERSION = '0.7.1'
 
 try:
     MYSMILE_THEME = Preferences.objects.filter(key='THEME').values_list('value', flat=True)[0]
